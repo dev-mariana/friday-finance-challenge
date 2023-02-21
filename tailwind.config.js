6968630a2bc52970dbef6298bee902a8d6a04161
mariana_bastos <maribastossrodrigues@gmail.com>
2023-02-21 11:34:43 -0300

    add tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./pages/**/*.vue"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
