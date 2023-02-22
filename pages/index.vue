9aea915517e520af4ecb76d87ed5a53fd7c18d9f
mariana_bastos <maribastossrodrigues@gmail.com>
2023-02-22 13:56:59 -0300

    add static home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,167 @@
 <template>
-  <div>
-    <h2>Transactions</h2>
+  <div class="container">
+    <h2
+      class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
+    >
+      Transactions
+    </h2>
+    <div class="flex flex-row flex-wrap gap-4 mb-4 mobile">
+      <div class="flex flex-col gap-3 flex-wrap mb-4 w-30 w-100">
+        <span>Search</span>
+        <input type="search" name="search" id="search" />
+      </div>
+      <div class="flex flex-col gap-3 flex-wrap mb-4 w-15 w-100">
+        <label for="" class="">Bank</label>
+        <select id="select-bank" name="select-bank" class="">
+          <option>USD</option>
+          <option>CAD</option>
+          <option>EUR</option>
+        </select>
+      </div>
+      <div class="flex flex-col gap-3 flex-wrap mb-4 w-15 w-100">
+        <label for="" class="">Account</label>
+        <select id="select-account" name="select-account" class="">
+          <option>USD</option>
+          <option>CAD</option>
+          <option>EUR</option>
+        </select>
+      </div>
+      <div class="flex flex-col gap-3 flex-wrap mb-4 w-15 w-100">
+        <span>Starting month</span>
+        <input type="date" name="" id="start-date" />
+      </div>
+      <div class="flex flex-col gap-3 flex-wrap mb-4 w-15 w-100">
+        <span>Ending month</span>
+        <input type="date" name="" id="end-date" />
+      </div>
+    </div>
+    <div class="table">
+      <table class="border border-slate-400 table-fixed">
+        <thead>
+          <tr class="border border-slate-300">
+            <th>Reference</th>
+            <th>Category</th>
+            <th>Date</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border border-slate-300">
+            <td class="text-center">Alfreds Futterkiste</td>
+            <td class="text-center">Maria Anders</td>
+            <td class="text-center">Germany</td>
+            <td class="text-center">Germany</td>
+          </tr>
+          <tr class="border border-slate-300">
+            <td class="text-center">Centro comercial Moctezuma</td>
+            <td class="text-center">Francisco Chang</td>
+            <td class="text-center">Mexico</td>
+            <td class="text-center">Mexico</td>
+          </tr>
+          <tr class="border border-slate-300">
+            <td class="text-center">Ernst Handel</td>
+            <td class="text-center">Roland Mendel</td>
+            <td class="text-center">Austria</td>
+            <td class="text-center">Austria</td>
+          </tr>
+          <tr class="border border-slate-300">
+            <td class="text-center">Island Trading</td>
+            <td class="text-center">Helen Bennett</td>
+            <td class="text-center">UK</td>
+            <td class="text-center">UK</td>
+          </tr>
+          <tr class="border border-slate-300">
+            <td class="text-center">Laughing Bacchus Winecellars</td>
+            <td class="text-center">Yoshi Tannamuri</td>
+            <td class="text-center">Canada</td>
+            <td class="text-center">Canada</td>
+          </tr>
+          <tr class="border border-slate-300">
+            <td class="text-center">Magazzini Alimentari Riuniti</td>
+            <td class="text-center">Giovanni Rovelli</td>
+            <td class="text-center">Italy</td>
+            <td class="text-center">Italy</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
+
+.container {
+  padding: 5vh;
+  background-color: #fff;
+  width: 100%;
+  margin: 0 auto;
+  margin-top: 5vh;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.w-30 {
+  width: 30%;
+}
+
+.w-15 {
+  width: 16%;
+}
+
+#search {
+  border: 1px solid grey;
+  border-radius: 3px;
+  padding: 5px;
+}
+
+#select-bank,
+#select-account,
+#start-date,
+#end-date {
+  padding: 6px;
+  border: 1px solid grey;
+  border-radius: 3px;
+}
+
+.text-2xl {
+  margin-bottom: 5vh;
+}
+
+table {
+  width: 100%;
+}
+
+thead {
+  background-color: #F7FAFC;
+}
+
+tbody {
+  background-color: #FFFFFF;
+}
+
+/* .table {
+  width: 100%;
+  display: table;
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+} */
+
+/* td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+} */
+
+@media screen and (max-width: 768px) {
+  .mobile {
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .w-100 {
+    width: 100%;
+  }
+}
 </style>
